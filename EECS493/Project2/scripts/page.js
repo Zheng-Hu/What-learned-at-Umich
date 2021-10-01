ab0208c0f4efd280b2960c594d32f2068dee9f9c
zheng hu <zhenghu@umich.edu>
2021-09-30 20:19:23 -0400

    InitialProject

---
+++ EECS493/Project2/scripts/page.js
@@ -0,0 +1,107 @@
+// ===================== Fall 2021 EECS 493 Assignment 2 =====================
+// This starter code provides a structure and helper functions for implementing
+// the game functionality. It is a suggestion meant to help you, and you are not
+// required to use all parts of it. You can (and should) add additional functions
+// as needed or change existing functions.
+
+// ==============================================
+// ============ Page Scoped Globals Here ========
+// ==============================================
+
+// Div Handlers
+let game_window;
+
+// Game Object Helpers
+let AST_OBJECT_REFRESH_RATE = 15;
+let maxPersonPosX = 1218;
+let maxPersonPosY = 658;
+let PERSON_SPEED = 5;                // Speed of the person
+let vaccineOccurrence = 20000;       // Vaccine spawns every 20 seconds
+let vaccineGone = 5000;              // Vaccine disappears in 5 seconds
+let maskOccurrence = 15000;          // Masks spawn every 15 seconds
+let maskGone = 5000;                 // Mask disappears in 5 seconds
+
+// Movement Helpers
+var LEFT = false;
+var RIGHT = false;
+var UP = false;
+var DOWN = false;
+var touched = false;
+
+// ==============================================
+// ============ Functional Code Here ============
+// ==============================================
+
+// Main
+$(document).ready(function () {
+    // ====== Startup ====== 
+    game_window = $('.game-window');
+    
+    // TODO: ADD MORE
+});
+
+// TODO: ADD YOUR FUNCTIONS HERE
+
+// Keydown event handler
+document.onkeydown = function(e) {
+    if (e.key == 'ArrowLeft') LEFT = true;
+    if (e.key == 'ArrowRight') RIGHT = true;
+    if (e.key == 'ArrowUp') UP = true;
+    if (e.key == 'ArrowDown') DOWN = true;
+}
+
+// Keyup event handler
+document.onkeyup = function (e) {
+    if (e.key == 'ArrowLeft') LEFT = false;
+    if (e.key == 'ArrowRight') RIGHT = false;
+    if (e.key == 'ArrowUp') UP = false;
+    if (e.key == 'ArrowDown') DOWN = false;
+}
+
+
+//===================================================
+
+// ==============================================
+// =========== Utility Functions Here ===========
+// ==============================================
+
+// Are two elements currently colliding?
+function isColliding(o1, o2) {
+  return isOrWillCollide(o1, o2, 0, 0);
+}
+
+// Will two elements collide soon?
+// Input: Two elements, upcoming change in position for the moving element
+function willCollide(o1, o2, o1_xChange, o1_yChange){
+  return isOrWillCollide(o1, o2, o1_xChange, o1_yChange);
+}
+
+// Are two elements colliding or will they collide soon?
+// Input: Two elements, upcoming change in position for the moving element
+// Use example: isOrWillCollide(paradeFloat2, person, FLOAT_SPEED, 0)
+function isOrWillCollide(o1, o2, o1_xChange, o1_yChange){
+  const o1D = { 'left': o1.offset().left + o1_xChange,
+        'right': o1.offset().left + o1.width() + o1_xChange,
+        'top': o1.offset().top + o1_yChange,
+        'bottom': o1.offset().top + o1.height() + o1_yChange
+  };
+  const o2D = { 'left': o2.offset().left,
+        'right': o2.offset().left + o2.width(),
+        'top': o2.offset().top,
+        'bottom': o2.offset().top + o2.height()
+  };
+  // Adapted from https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+  if (o1D.left < o2D.right &&
+    o1D.right > o2D.left &&
+    o1D.top < o2D.bottom &&
+    o1D.bottom > o2D.top) {
+     // collision detected!
+     return true;
+  }
+  return false;
+}
+
+// Get random number between min and max integer
+function getRandomNumber(min, max){
+  return (Math.random() * (max - min)) + min;
+}
