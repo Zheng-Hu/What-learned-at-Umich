572d10dc81b794c52176b8e329b8b3cad354e86f
zheng hu <zhenghu@umich.edu>
2021-10-30 21:00:32 -0400

    Finish basic UI format

--- EECS493/Project3/script.js
+++ EECS493/Project3/script.js
@@ -2,22 +2,40 @@ new Vue({
   el: '#app',
   data () {
     return {
+      tabNum:0,
       info: null,
       searchValue:'',
-      artist1: './img/1.jpg',
-      artist2: './img/2.jpg',
+      artistNum: 0,
+      resultList:[],
+      buttonNum:0,
     }
   },
   methods:{
-    searchHandler() {
-      axios
-      .get('https://itunes.apple.com/search?term=' + self.searchValue + '.')
-      .then(response => (this.info = response))
-      console.log(this.info);
-    }
+    async searchHandler() {
+      console.log(this.searchValue);
+      await axios
+      .get('https://itunes.apple.com/search?term=' + this.searchValue + '.')
+      .then(response => (this.info = response));
+      console.log(this.info)
+      if(this.info.data.resultCount == 0) {
+        this.artistNum = 0;
+        // alert("No artist found");
+      } else {
+        this.artistNum = this.info.data.resultCount;
+        this.resultList = this.info.data.results;
+      }
+    },
+
+    filterButtonHandler(buttonIndex){
+      console.log(this.resultList[0].primaryGenreName)
+      if(buttonIndex == 1) {this.resultList = (this.resultList).filter(item => item.primaryGenreName == "New Age")};
+      if(buttonIndex == 2) {this.resultList = (this.resultList).filter(item => item.primaryGenreName == "Hip-Hop/Rap")};
+      if(buttonIndex == 3) {this.resultList = (this.resultList).filter(item => item.primaryGenreName == "Pop")};
+      if(buttonIndex == 4) {this.resultList = (this.resultList).filter(item => item.primaryGenreName == "Alternative")};
+      if(buttonIndex == 5) {this.resultList = (this.resultList).filter(item => item.primaryGenreName == "Electronic")};
+      if(buttonIndex == 6) {this.resultList = (this.resultList).filter(item => item.primaryGenreName == "Dance")};
+
+    },
   },
-  mounted () {
-    
-  }
 })
 
